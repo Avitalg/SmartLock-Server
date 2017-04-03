be3ac500eea4861ac1c6db2ff714b484a1aa9027
Avital Glazer <avitalg@BETAMEDIA.LOCAL>
2017-04-03 17:22:48 +0300

    user

--- permission/schema.js
+++ permission/schema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var PermissionSchema = new schema({
-    userid: {type: Number, required:true},
+    userid: {type: String, required:true},
     lockid: {type: String, required:true},
     physicalId: {type: String},
     frequency: { type:String, required:true},
