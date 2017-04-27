7ca3b886d9cadf6add1b0b551f0b8c0ed492ad46
Avital <avitalg91@gmail.com>
2017-04-27 11:21:50 +0300

    get user bug

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var users = require('../middlewares/user');
 
 module.exports = function(app){
     app.get('/api/getUsers', users.getUsers);
-    app.get('/api/getUser/:userid', users.getUser);
+    app.get('/api/getUser/:username', users.getUser);
     app.post('/api/addUser', users.addUser);
     app.delete('/api/removeUser/:userid', users.removeUser);
     app.put('/api/updateUser/:userid/:username/:phone/:password', users.updateUser);
