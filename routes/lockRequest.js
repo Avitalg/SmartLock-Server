045e768efdeda2c08d7e11e7c6e0bd8100fcf18c
Avital <avitalg91@gmail.com>
2017-05-10 22:36:52 +0300

    finger print permission

--- routes/lockRequest.js
+++ routes/lockRequest.js
@@ -5,7 +5,7 @@ var lockRequest = require('../middlewares/lockRequest');
 var permissions = require('../middlewares/permission');
 
 module.exports = function(app) {
-    app.post('/api/requestLockAction/:action',permissions.checkPermission, lockRequest.requestLockAction);
+    app.post('/api/requestLockAction/:action',permissions.rightPermission, lockRequest.requestLockAction);
     app.get('/api/checkLockAction/:requestId',lockRequest.checkLockAction);
     app.get('/api/lockRequest/:lockId',lockRequest.checkLockRequest);
     app.post('/api/lockRequest/:lockId',lockRequest.updateLockRequest);
