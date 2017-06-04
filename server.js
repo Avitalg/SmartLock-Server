b8b55426a603884054369091fe3078ad98106118
Avital <avitalg91@gmail.com>
2017-06-04 21:41:15 +0300

    test session

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ var cors = require('cors')
 
 process.env.ENV_VAR = process.env.ENV_VAR || "qa";
 
+let whitelist = ['https://smartlockproj.com','https://www.smartlockproj.com'];
+var corsOptions = {
+    origin: (origin:any, callback:any)=>{
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+
+
 var db = require('./database');
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -32,7 +44,7 @@ var port = process.env.PORT || 3000;
 app.set('port', port);
 app.use('/', express.static('./public'));
 app.use(function(req, res, next){
-	res.header('Access-Control-Allow-Origin', "https://smartlockproj.com");
+	res.header("Access-Control-Allow-Credentials", "true");
 	res.header('Access-Control-Allow-Header', "Origin, X-Requested-With, Content-Type, Accept");
 	app.set('json spaces', 4);
 	res.set('Content-Type', "application/json");
