ca1fc5ccb442a05dab7426806c000a539064d3c2
Avital Glazer <avitalg@BETAMEDIA.LOCAL>
2017-06-05 21:26:04 +0300

    db session

--- routes/user.js
+++ routes/user.js
@@ -9,9 +9,8 @@ module.exports = function(app){
     app.get('/api/getUsersByLock/:lockid', permissions.getPermissionsByLock, users.getUsersByLock);
     app.post('/api/addUser', users.addUser);
     app.post('/api/addUserPhoto', users.addUserPhoto);
-    app.post('/api/login', users.login)
+    app.post('/api/login', users.login);
     app.delete('/api/removeUser/:username', permissions.removeUserPermissions, users.removeUser);
     app.put('/api/updateUser/:username/:nusername/:phone', users.updateUser, permissions.changePermissionUsername);
     app.put('/api/changePassword/:username/:password', users.changePassword);
-
 };
