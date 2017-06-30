13399ed2fe890c209be6937a43c8d99299a3309a
Avital <avitalg91@gmail.com>
2017-06-30 19:30:53 +0300

    contact us

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/', express.static('./public'));
 app.use(function(req, res, next){
-	var headerOrigin = (process.env.ENV_VAR == 'development') ? "http://localhost:8887" : req.headers.origin;
 	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header('Access-Control-Allow-Origin',  headerOrigin);
+	res.header('Access-Control-Allow-Origin',  req.headers.origin);
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 	app.set('json spaces', 4);
