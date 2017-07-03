66585dead2c6065cf8ac498b93e2f8fe9b926329
Avital <avitalg91@gmail.com>
2017-07-03 18:06:16 +0300

    revert

--- models/user.js
+++ models/user.js
@@ -49,60 +49,6 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
     });
 };
 
-// Send a verification token to this user
-userSchema.methods.sendAuthyToken = function(cb) {
-    var self = this;
-
-    if (!self.authyId) {
-        // Register this user if it's a new user
-        authy.register_user(self.email, self.phone, self.countryCode,
-            function(err, response) {
-            if (err || !response.user) return cb.call(self, err);
-            self.authyId = response.user.id;
-            self.save(function(err, doc) {
-                if (err || !doc) return cb.call(self, err);
-                self = doc;
-                sendToken();
-            });
-        });
-    } else {
-        // Otherwise send token to a known user
-        sendToken();
-    }
-
-    // With a valid Authy ID, send the 2FA token for this user
-    function sendToken() {
-        authy.request_sms(self.authyId, true, function(err, response) {
-            cb.call(self, err);
-        });
-    }
-};
-
-// Test a 2FA token
-userSchema.methods.verifyAuthyToken = function(otp, cb) {
-    const self = this;
-    authy.verify(self.authyId, otp, function(err, response) {
-        cb.call(self, err, response);
-    });
-};
-
-userSchema.methods.sendMessage =
-  function(message, successCallback, errorCallback) {
-      const self = this;
-      const toNumber = `+972${self.phone}`;
-      console.log("sms sent to "+toNumber);
-      twilioClient.messages.create({
-          to: toNumber,
-          from: config.twilioNumber,
-          body: message,
-      }).then(function() {
-        successCallback();
-      }).catch(function(err) {
-        errorCallback(err);
-      });
-  };
-
-
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
