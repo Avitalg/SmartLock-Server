f37f46a7c76f459eaff42be79ce67c627051fe99
Avital <avitalg91@gmail.com>
2019-04-12 23:07:12 +0300

    Update main.js

--- config/main.js
+++ config/main.js
@@ -2,8 +2,8 @@ process.env.ENV_VAR = process.env.ENV_VAR || "development";
 var port = process.env.PORT || 3000;
 
 var config={
-	prodMongoUrl: 'mongodb://admin:Aa1234@ds147079.mlab.com:47079/final_project',
-	devMongoUrl : 'mongodb://admin:191923@ds159180.mlab.com:59180/final_project-test'
+	prodMongoUrl: 'mongodb://[user]:[pass]@ds147079.mlab.com:47079/final_project',
+	devMongoUrl : 'mongodb://[user]:[pass]@ds159180.mlab.com:59180/final_project-test'
 };
 
 var mongoUrl = (process.env.ENV_VAR == 'development') ? config.devMongoUrl : config.prodMongoUrl;
